9a9b7f91673f9ccd1858d423dabec276ee88ad17
Albert <agent0308@gmail.com>
2020-02-24 22:26:42 +0300

    Пофиксил небольшую ошибку в шапке с условным рендерингом

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,14 +2,10 @@
     <div>
         <table>
             <tr>
-                <td>
-                    <label>2</label>
-                </td>
-                <td>
-                    <a style="color:white" href="/personalcabinet" v-if="localStorage.getItem('token')">{{name}}</a>
+                <td align="right">
+                    <a style="color:white" href="/personalcabinet" v-if="tokenized">Альберт</a>
                     <a href="/registration" v-else>Нет аккаунта? Зарегистрируйтесь!</a>
                 </td>
-
             </tr>
         </table>
     </div>
@@ -23,6 +19,12 @@
                 name: undefined,
             }
         },
+        computed: {
+            tokenized: function () {
+                return localStorage.getItem("token");
+            }
+        },
+
         methods: {
             beforeUpdate: function () {
             }
@@ -33,7 +35,11 @@
 <style scoped>
     div {
         background: violet;
-        padding: -10%;
+        padding-top: 1%;
         text-align: right;
+    }
+
+    td {
+        alignment: right;
     }
 </style>
