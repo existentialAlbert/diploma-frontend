991ae4bce6323f271019f981bb0a63e390e57445
Albert <agent0308@gmail.com>
2020-03-06 14:14:27 +0300

    Добавил html для страницы с таском

---
+++ src/components/Task.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <h1>Название таска</h1>
+        <article style="text-align: left; padding-left: 25%">
+            Описание таска
+        </article>
+        <form>
+            <label>
+                <textarea cols="100" rows="30" v-model="userAnswer" style="width:560px"> </textarea>
+            </label>
+        </form>
+        {{userAnswer}}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Task",
+        data: function () {
+            return {
+                taskInfo: [],
+                userAnswer: "",
+
+            }
+        },
+        methods: {
+            check() {
+
+            },
+        },
+        created() {
+            const axios = require('axios').default;
+            axios({
+                url: ""
+            }).then(response => {
+                response;
+            });
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import UsersPage from "@/components/UsersPage";
 import Registration from "@/components/Registration";
 import PersonalCabinet from "@/components/PersonalCabinet";
 import TasksPage from "@/components/TasksPage";
+import Task from "@/components/Task";
 
 Vue.use(VueRouter);
 
@@ -14,7 +15,8 @@ const routes = [
     {path: '/registration', component: Registration},
     {path: '/users/user/:username', component: PersonalCabinet},
     {path: '/users/page/:page', component: UsersPage},
-    {path: '/tasks/page/:page', component: TasksPage}
+    {path: '/tasks/page/:page', component: TasksPage},
+    {path: '/tasks/task/:task_id', component: Task}
 ];
 const axios = require('axios').default;
 const token = localStorage.getItem('token');
